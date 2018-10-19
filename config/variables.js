af27f4c4d8868402aa5b7717de0f2144404ec366
Renato Oliveira <renato.rops@gmail.com>
2018-10-19 17:59:16 -0300

    Add boilerplate Juca faltando path publico

---
+++ config/variables.js
@@ -0,0 +1,9 @@
+const pjson = require('../package.json');
+
+module.exports = {
+  PROJECT_ID: "cyclone",
+  TEMPLATE_PREFIX: "eCore-",
+  PROJECT_VERSION: pjson.version,
+  STORE_ID: "cyclone",
+  STORE_NAME: "cyclone"
+}
