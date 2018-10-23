8f11004c3d5038a47fa86a2a175f7f4626952daf
Renato Oliveira <renato.rops@gmail.com>
2018-10-23 12:12:12 -0300

    Alteracao na msg do cep do checkout

--- src/js/eCore-novo-ambiente.js
+++ src/js/eCore-novo-ambiente.js
@@ -1,14 +1,12 @@
-	// PRODUTO
-	$(document).ready(function() {
-    setTimeout(()=> {
-      $('#fraseEntrega p').html(
-          '<style>' +
-        ' .item-unavailable{font-size: 12px !important;font-weigth: unset !important;padding-left:6px;}'+
-          '</style>'+
-          '    <span class="help-arrow top-arrow"></span>	' +
-          '        <span data-bind="visible: unavailableForDelivery" data-i18n="cart.unavailableForDelivery">E-commerce de produtos Herman Miller disponível apenas para os estados de SP e RJ.' +
-          '        Para os demais estados entre em contato através dos seguintes canais:<br/>' +
-          '        Tel (21) 2543-2136 | (21) 96772-5927 WhatssApp <br> <span style="color: #08c; font-weight: bold;">ecommerce@novoambiente.com</span>' +
-          '    </span>');
-      }, 5000);
-  });
+// PRODUTO
+$(document).ready(function () {
+	$('#fraseEntrega p').html(
+		'<style>' +
+		' .cep-invalido p > span{font-size: 12px !important; color: #000; font-weigth: unset !important;padding-left:6px;}' +
+		'</style>' +
+		'    <span class="help-arrow top-arrow"></span>	' +
+		'        <span data-bind="visible: unavailableForDelivery" data-i18n="cart.unavailableForDelivery">E-commerce de produtos Herman Miller disponível apenas para os estados de SP e RJ.' +
+		'        Para os demais estados entre em contato através dos seguintes canais:<br/>' +
+		'        Tel (21) 2543-2136 | (21) 96772-5927 WhatssApp <br> <span style="color: #08c; font-weight: bold;">ecommerce@novoambiente.com</span>' +
+		'    </span>')
+});
